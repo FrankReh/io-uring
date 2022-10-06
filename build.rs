2354a36e4e50705ccca74d19688730b55254a370
Frank Rehwinkel <frankrehwinkel@gmail.com>
2022-10-06 11:58:07 -0400

    Modify build.rs to use hdr/io_uring.h
    This is still a bit of a hack, as I don't know if this
    will be accepted upstream. So the original line
    for including linux/io_uring.h is still shown, but commented.
    
    And a builder clang_arg of "-F." is added to get clang
    to discover the local header.
    
    The intention is that the src/sys/sys.rs can be built by running
    
        cargo build --all-features
    
    regardless of the version of Linux the build is happening on.

--- build.rs
+++ build.rs
@@ -12,8 +12,9 @@ fn main() {
 #include <linux/time_types.h>
 #include <linux/stat.h>
 #include <linux/openat2.h>
-#include <linux/io_uring.h>
+#include <hdr/io_uring.h>
     "#;
+//#include <linux/io_uring.h>
 
     #[cfg(not(feature = "overwrite"))]
     let outdir = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -22,6 +23,7 @@ fn main() {
     let outdir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/sys");
 
     bindgen::Builder::default()
+        .clang_arg("-F.")
         .header_contents("include-file.h", INCLUDE)
         .ctypes_prefix("libc")
         .prepend_enum_name(false)
